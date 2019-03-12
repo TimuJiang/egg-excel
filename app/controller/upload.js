2a7782977960114e1ae147f04a279edf9e7a6690
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-03-12 20:10:00 +0800

    wip

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -1,9 +1,20 @@
 const path = require('path');
-const sendToWormhole = require('stream-wormhole');
 const Controller = require('egg').Controller;
 
-const Excel = require('exceljs');
 const XLSX = require('xlsx');
+
+async function formatExcel(workbook) {
+	let {SheetNames, Sheets} = workbook
+	let list = []
+	Object.values(Sheets).forEach(function(item){
+		list.push(XLSX.utils.sheet_to_json(item, {header: "A"}))
+	});
+	return {
+		list,
+		SheetNames
+	}
+}
+
 
 class UploadController extends Controller {
 	async upload() {
@@ -11,15 +22,12 @@ class UploadController extends Controller {
 		const file = ctx.request.files[0];
 		const name = 'egg-multipart-test/' + path.basename(file.filename);
 		let workbook = XLSX.readFile(file.filepath)
-		let {SheetNames, Sheets} = workbook
-		let sheet = Sheets[SheetNames[0]]
-		let list = XLSX.utils.sheet_to_json(sheet, {header: 1})
+		let data = await formatExcel(workbook)
 		ctx.body = {
 			code: 'success',
 			data:{
 				url: name,
-				sheetNames: SheetNames,
-				sheets: list
+				...data
 			},
 			message: 'message'
 		};
