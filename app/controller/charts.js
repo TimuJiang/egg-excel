4433566e88570008c39c34489851e15fe308c2da
xiaosong.jiang <xiaosong.jiang@geely.com>
2019-05-06 17:37:36 +0800

    wip

--- app/controller/charts.js
+++ app/controller/charts.js
@@ -7,11 +7,37 @@ class ChartsController extends Controller {
 
 		ctx.body = {
 			code: 'success',
-			data: Mock.mock({
-				'list|10': [{
+			... Mock.mock({
+				'data|10': [{
 					'id|+1': 1,
 					'name': '@cname',
 					'value|200-1000': 1
+				}]
+			}),
+			message: 'message'
+		};
+	}
+	async getBorad() {
+		const {ctx} = this;
+		ctx.body = {
+			code: 'success',
+			... Mock.mock({
+				'data|10': [{
+					grid: { x: 0, y: 0, width: 380, height: 280, zIndex: null },
+					'id|+1': 1,
+					'reportDataBaseId|+1 ': 1,
+					'resourceId|+1': 1,
+					resourceType: 1,
+					style: '{showTitle: true}',
+					type: '1040',
+					typeName: 'g2-bar',
+					uid: 'uid-1556605339779-2',
+					data: {
+						content: '',
+						dimension: [{ meta: 'name' }],
+						legend: [],
+						measure: [{ meta: 'value' }]
+					}
 				}]
 			}),
 			message: 'message'
--- app/router.js
+++ app/router.js
@@ -8,4 +8,5 @@ module.exports = app => {
   router.get('/', controller.home.index);
   router.post('/geely-dataview/dataSource/upload',controller.upload.upload);
   router.post('/geely-dataview/borad/charts',controller.charts.getCharts);
+  router.post('/geely-dataview/borad/get',controller.charts.getBorad);
 };
